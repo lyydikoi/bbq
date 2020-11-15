849c4234b74816398999c8d54a49338253fa719b
Sergei Kasianov <lyydikoi@gmail.com>
2020-11-15 20:28:57 +0200

    Devise and auth ready...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-
+  before_action :set_user, only: [:show]
+  before_action :set_current_user, except: [:show]
+  before_action :authenticate_user!, except: [:show]
+  
   # GET /users/1
   # GET /users/1.json
   def show
@@ -23,12 +25,15 @@ class UsersController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
+  
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
+  def set_current_user
+    @user = current_user
+  end
+
   def user_params
     params.require(:user).permit(:name, :email)
   end
