bdece63006d96e5619eca2175b1ecaca19b1d95b
Sergei Kasianov <lyydikoi@gmail.com>
2020-12-20 15:13:00 +0200

    Hw-57-2: Prevent sending notification email to author of comment.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
     @new_comment.user = current_user
 
     if @new_comment.save
-      notify_subscribers(@event, @new_comment)
+      notify_other_subscribers(@event, @new_comment)
       
       redirect_to @event, notice: I18n.t('controllers.comments.created')
     else
@@ -29,8 +29,9 @@ class CommentsController < ApplicationController
 
   private
 
-  def notify_subscribers(event, comment)
+  def notify_other_subscribers(event, comment)
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+    all_emails.delete_if {|email| email == current_user&.email }
   
     all_emails.each do |mail|
       EventMailer.comment(event, comment, mail).deliver_now
