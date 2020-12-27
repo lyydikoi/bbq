dc84c55d8803757f14be6dc325989025120f4a12
Sergei Kasianov <lyydikoi@gmail.com>
2020-12-28 00:18:14 +0200

    Hw-58-2: Add JS gallery.

--- app/frontend/js/site.js
+++ app/frontend/js/site.js
@@ -1,7 +1,7 @@
 import initMap from '../js/init_map'
 import initDatePicker from '../js/init_date_picker'
 import 'bootstrap'
-//import 'ekko-lightbox/dist/ekko-lightbox'
+import 'ekko-lightbox/dist/ekko-lightbox'
 import "@fortawesome/fontawesome-free/js/all";
 
 window.addEventListener("turbolinks:load", () => {
@@ -9,7 +9,7 @@ window.addEventListener("turbolinks:load", () => {
   initDatePicker();
 })
 
-/*$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
   event.preventDefault();
   $(this).ekkoLightbox();
-});*/
+});
